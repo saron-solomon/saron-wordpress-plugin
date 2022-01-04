ef7c061ab32eef414b9699986d9861ae1494cd1a
saron-solomon <it.saron.solomon@gmail.com>
2022-01-04 20:22:59 +0300

    plugin demo started

---
+++ demo.plugin.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+* Plugin Name: Demo Plugin 
+* Plugin URI: http://demoplugin.com
+* Description: This is a demo plugin developed forlearning purpose 
+* Version: 1.0.0
+* Author: saron solomon.
+* Author URI: http://saron.com
+* License: GPL2
+*/
+
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Hey , you can not access  this file silly human ' );
+}
+
+// other way 
+// defined( 'ABSPATH' ) or 	die( 'Hey , you can not access  this file dud ' );
+
+//other way ...
+// if (function_exists('add_action')){ echo "You can not access this file";}
+
+// The Plugin class 
+class DemoPlugin{
+
+    function activate()
+    {
+    // Register custom PT
+    // Flush 
+    }
+    
+    function deactivate()
+    {
+    // Flush 
+    }
+    
+    function uninstall()
+    {
+    //delete CP , delete tables on DB
+    }
+
+     
+
+ 
+
+
+
+}
+if(class_exists('DemoPlugin'))
+{
+    $demoplugin = new DemoPlugin();
+}
+
+// Registering Activation 
+register_activation_hook(__FILE__, array( $demoplugin,'activate'));
+
+// Registering De Activation 
+register_deactivation_hook(__FILE__, array( $demoplugin,'deactivate'));
+
+function dp_add_form(){
+    require_once plugin_dir_path(__FILE__)."setting/contact.php";
+}
+
+add_shortcode("form_1", "dp_add_form");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>s
