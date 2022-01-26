a36d46b002c2adcef2eae68292e9658a9ca15f05
saron-solomon <it.saron.solomon@gmail.com>
2022-01-26 09:59:41 +0300

    custom title widget edited

--- extinisible-changes.php
+++ extinisible-changes.php
@@ -14,6 +14,25 @@ function hd_test_edit_social_profile( $profiles ) {
 		unset( $profiles['linkedin'] );
 	}
 
+	// add the twitter profile.
+	$profiles['pinterest'] = array(
+		'id'                => 'hd_espw_pinterest_url',
+		'label'             => __( 'Pinterest URL', 'hd-extensible-social-profiles-widget' ),
+		'class'             => 'pinterest',
+		'description'       => __( 'Enter your Pinterest profile URL', 'hd-extensible-social-profiles-widget' ),
+		'priority'          => 50,
+		'type'              => 'text',
+		'default'           => '',
+		'sanitize_callback' => 'sanitize_text_field',
+	);
+
+	// return profiles;
+	return $profiles;
+
+}
+
+add_filter( 'hd_espw_social_profiles', 'hd_test_edit_social_profile', 20, 1 );
+
 /**
  * Removes the built in title output.
  */
